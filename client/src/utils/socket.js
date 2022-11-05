eba881d4925c31cac0da26208560b952c4b0378b
streaker303 <wangjian@threatbook.cn>
2022-11-05 18:11:55 +0800

    变量

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 let host = location.origin;
-console.log(host, '本机')
-const socket = io.connect('https://192.168.63.58:9000');
+console.log(location, '本机')
+const socket = io.connect(`https://${location.hostname}:9000`);
 export default socket;
